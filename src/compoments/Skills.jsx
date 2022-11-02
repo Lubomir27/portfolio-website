077abdc4db5862b25c878dc506960a436e7ea573
Lubomir27 <102256957+Lubomir27@users.noreply.github.com>
2022-11-02 19:56:54 +0100

    eslint1

--- src/compoments/Skills.jsx
+++ src/compoments/Skills.jsx
@@ -8,7 +8,6 @@ import GitHub from "../assets/github.png";
 const Skills = () => {
   return (
     <div name="skills" className="w-full h-screen  text-gray-300 ">
-      {/*Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
