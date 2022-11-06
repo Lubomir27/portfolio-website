7a15476b8f24c6efbc519c45ead348c1973d4b14
Lubomir27 <102256957+Lubomir27@users.noreply.github.com>
2022-11-06 19:27:53 +0100

    changed order

--- src/compoments/Work.jsx
+++ src/compoments/Work.jsx
@@ -27,6 +27,25 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
           <div
+            style={{ backgroundImage: `url(${mobis})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Mobis s.r.o
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://mobis.cz/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
             style={{ backgroundImage: `url(${medicPage})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
@@ -55,25 +74,6 @@ const Work = () => {
               </span>
               <div className="pt-8 text-center">
                 <a href="https://www.timiscooking.sk/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Live
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${mobis})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Mobis s.r.o
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://mobis.cz/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Live
                   </button>
